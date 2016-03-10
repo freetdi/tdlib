8d8756522b3f488de1230980b53f73e60b8b2ec7
Felix Salfelder <felix@salfelder.org>
2016-03-10 20:43:01 +0100

    move MD details to TD_elimination_orderings.hpp

--- src/TD_elimination_orderings.hpp
+++ src/TD_elimination_orderings.hpp
@@ -71,6 +71,32 @@
 #include "TD_std.hpp"
 
 namespace treedec{
+namespace detail{
+
+    // DRAFT. no useful interface
+template<typename G>
+struct degree_mod : public noboost::vertex_callback<G>{
+    typedef typename boost::graph_traits<G>::vertex_descriptor vertex_descriptor;
+    degree_mod(misc::DEGS<G>* d, G* g) : _degs(d), _g(g){}
+
+    // reinsert with degree-1
+    void operator()(vertex_descriptor v){ untested();
+        unsigned deg=boost::degree(v,*_g);
+        (void)deg;
+        assert(deg);
+        bool done=(*_degs)[deg-1].insert(v).second;
+        (void)done;
+        assert(done);
+    }
+    //private: not yet.
+//    BUG:: hardcoded type
+        misc::DEGS<G>* _degs;
+    private:
+        degree_mod(const degree_mod&){}
+        G* _g;
+};
+
+} //namespace detail
 
 //Constructs a tree decomposition from the elimination ordering obtained by the
 //minimum-degree heuristic. Ignores isolated vertices.
