39230938ccafe29be856117999f620a34f58b8c0
Felix Salfelder <felix@salfelder.org>
2016-04-09 02:14:03 +0100

    testing

--- src/TD_degree.hpp
+++ src/TD_degree.hpp
@@ -67,8 +67,8 @@ public: // picking
     }
     // pick a minimum degree vertex within degree range [lower, upper]
     std::pair<vertex_descriptor,degree_t> pick_min(unsigned lower=0, unsigned upper=-1) const
-    { untested();
-        while(_degs[lower].empty()){ untested();
+    {
+        while(_degs[lower].empty()){
             ++lower;
             // min_ntd==num_vert contradicts the outer loop condition
             // (this loop should be safe)
