f90d99d0d884cd4601c7cd40a9622045457aa774
Felix Salfelder <felix@salfelder.org>
2016-04-18 18:29:34 +0100

    fix PP_FI_TM pytdlib test
    (need to pass a copy)

--- src/TD_combinations.hpp
+++ src/TD_combinations.hpp
@@ -122,7 +122,9 @@ void PP_FI_TM(G_t &G, T_t &T, int &low){
         typename std::vector<typename boost::graph_traits<G_t>::vertex_descriptor> old_elim_ordering;
         typename std::vector<typename boost::graph_traits<G_t>::vertex_descriptor> new_elim_ordering;
 
+        G_t H(G);
         treedec::fillIn_ordering(G, old_elim_ordering, true); //true = ignore isolated vertices
+        G = H; // reset
         treedec::minimalChordal(G, old_elim_ordering, new_elim_ordering);
         treedec::ordering_to_treedec(G, new_elim_ordering, T, true); //true = ignore isolated vertices
     }
@@ -140,7 +142,9 @@ void FI_TM(G_t &G, T_t &T){
 
     typename std::vector<typename boost::graph_traits<G_t>::vertex_descriptor> old_elim_ordering;
     typename std::vector<typename boost::graph_traits<G_t>::vertex_descriptor> new_elim_ordering;
+    G_t H(G);
     treedec::fillIn_ordering(G, old_elim_ordering);
+    G = H; // reset
     treedec::minimalChordal(G, old_elim_ordering, new_elim_ordering);
     treedec::ordering_to_treedec(G, new_elim_ordering, T);
 }
