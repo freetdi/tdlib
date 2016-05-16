ed2b3dae410d40a7f649780b34bc38e3348d549f
Felix Salfelder <felix@salfelder.org>
2016-05-16 09:26:59 +0100

    remove fill. rename fill2 to fill
    (provide a wrapper, for later removal)

--- src/elimination_orderings.hpp
+++ src/elimination_orderings.hpp
@@ -292,82 +292,6 @@ template <typename G_t, typename T_t>
 size_t /*FIXME*/ fillIn_decomp(G_t &G, T_t *T)
 #endif
 {
-    std::vector<typename noboost::treedec_traits<T_t>::bag_type> bags;
-    std::vector<typename noboost::treedec_traits<T_t>::bag_type::value_type> elim_vertices;
-    typename boost::graph_traits<G_t>::vertices_size_type nv=boost::num_vertices(G);
-    unsigned upper_bound=0;
-
-    if(T){
-        bags.resize(nv);
-        elim_vertices.resize(nv);
-    }
-
-    unsigned int i = 0;
-    while(boost::num_edges(G) > 0){
-        //Search a vertex v such that least edges are missing for making the
-        //neighbourhood of v a clique.
-
-        typename boost::graph_traits<G_t>::vertex_iterator vIt, vEnd;
-        boost::tie(vIt, vEnd) = boost::vertices(G);
-        typename boost::graph_traits<G_t>::vertex_descriptor min_vertex = *vIt;
-
-        unsigned int min_fill = UINT_MAX;
-        for(; vIt != vEnd; vIt++){
-            if(boost::degree(*vIt, G) == 0){
-                continue;
-            }
-
-            unsigned current_fill=treedec::count_missing_edges(*vIt, G);
-
-            if(current_fill < min_fill){
-                min_fill = current_fill;
-                min_vertex = *vIt;
-                if(current_fill == 0){
-                    break;
-                }
-            }
-        }
-
-        if(T){
-            typename boost::graph_traits<G_t>::adjacency_iterator nIt, nEnd;
-            for(boost::tie(nIt, nEnd) = boost::adjacent_vertices(min_vertex, G); nIt != nEnd; nIt++){
-                bags[i].insert((typename noboost::treedec_traits<T_t>::bag_type::value_type) *nIt);
-            }
-        }
-        else if(boost::degree(min_vertex, G) > upper_bound){
-            upper_bound = boost::degree(min_vertex, G);
-        }
-
-        noboost::make_clique(boost::adjacent_vertices(min_vertex, G), G); //replace this with make_clique_and_hijack?!
-
-        if(T){
-            elim_vertices[i++] = min_vertex;
-        }
-
-        boost::clear_vertex(min_vertex, G);
-    }
-
-    if(T){
-        for(; i > 0; i--){
-            treedec::glue_bag(bags[i-1], elim_vertices[i-1], *T);
-        }
-        return 0;
-    }
-    else{
-        return upper_bound;
-    }
-}
-
-//Construct a tree decomposition from the elimination ordering obtained by the
-//fill-in heuristic. Ignores isolated vertices.
-#if __cplusplus >= 201103L
-template <typename G_t, typename T_t=typename noboost::treedec_chooser<G_t>::type>
-size_t /*FIXME*/ fillIn_decomp2(G_t &G, T_t *T=NULL)
-#else
-template <typename G_t, typename T_t>
-size_t /*FIXME*/ fillIn_decomp2(G_t &G, T_t *T)
-#endif
-{
     typedef typename noboost::treedec_chooser<G_t>::value_type my_vd;
     typedef typename boost::graph_traits<G_t>::vertex_descriptor vertex_descriptor;
     typedef typename noboost::fill_chooser<G_t>::type fill_type;
@@ -462,15 +386,6 @@ size_t /*FIXME*/ fillIn_decomp(G_t &G)
 }
 #endif
 
-#if __cplusplus < 201103L
-template <typename G_t>
-size_t /*FIXME*/ fillIn_decomp2(G_t &G)
-{ untested();
-    return fillIn_decomp2(G, (typename noboost::treedec_chooser<G_t>::type*)NULL);
-}
-#endif
-
-
 } //namespace impl
 
 
@@ -491,21 +406,11 @@ void fillIn_decomp(G_t &G, T_t &T){
     treedec::preprocessing_glue_bags(bags, T);
 }
 
-//Construct a tree decomposition from the elimination ordering obtained by the
-//fill-in heuristic.
+// transition, remove.
 template <typename G_t, typename T_t>
-void fillIn_decomp_exp(G_t &G, T_t &T){
-    if(boost::num_vertices(G) == 0){
-        boost::add_vertex(T);
-        return;
-    }
-
-    std::vector< boost::tuple<typename noboost::treedec_traits<T_t>::bag_type::value_type,
-                              typename noboost::treedec_traits<T_t>::bag_type> > bags;
-
-    treedec::Islet(G, bags);
-    impl::fillIn_decomp2(G, &T);
-    treedec::preprocessing_glue_bags(bags, T);
+void fillIn_decomp_exp(G_t &G, T_t &T)
+{
+    return fillIn_decomp(G, T);
 }
 
 
