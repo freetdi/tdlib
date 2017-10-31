e5031814648128870a1b3c73efa1dd20d60dac52
Felix Salfelder <felix@salfelder.org>
2017-10-31 09:30:21 +0000

    fix qualification in boost calls

--- src/thorup.hpp
+++ src/thorup.hpp
@@ -267,8 +267,8 @@ void make_clique(const std::set<unsigned int> &X , G_t &G)
   std::set<unsigned int>::const_iterator n1, n2;
   for (n1 = X.begin(); n1 != X.end(); n1++){
     for (n2 = n1, ++n2; n2 != X.end(); ++n2){
-      if(!edge(*n1, *n2, G).second){
-			add_edge(*n1, *n2, G);
+      if(!boost::edge(*n1, *n2, G).second){
+			boost::add_edge(*n1, *n2, G);
 		}else{
 		}
 	 }
