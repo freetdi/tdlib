64fb21dfb963245c137b4e145921b7eaae45d701
Lukas Larisch <lukas.larisch@kaust.edu.sa>
2018-05-16 21:19:10 -0400

    licencing, version 0.9

--- src/validation.hpp
+++ src/validation.hpp
@@ -1,4 +1,4 @@
-// Lukas Larisch, 2014 - 2016
+// Lukas Larisch, 2014 - 2017
 //
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the
