9f6acbab1ad09351e0dcfe80bda99db4302b8175
Jerry James <loganjerry@gmail.com>
2019-12-17 17:11:04 -0700

    Update the FSF address

--- src/deprecated/approximation.hpp
+++ src/deprecated/approximation.hpp
@@ -12,7 +12,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// Foundation, 51 Franklin Street - Suite 500, Boston, MA 02110-1335, USA.
 //
 //
 // A tree decomposition is a graph that has a set of vertex indices as bundled property, e.g.:
