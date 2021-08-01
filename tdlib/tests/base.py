add28fbc1b3e09db5ea48836b9a2f724fdde01af
Felix Salfelder <felix@salfelder.org>
2021-08-02 00:00:00 +0100

    long checks
    - readd custom test-driver
    - avoid Stdcbench, it's not there

--- tdlib/tests/base.py
+++ tdlib/tests/base.py
@@ -12,7 +12,7 @@ def print_graph_name(PREFIX, c):
     import Zoo
     import Dimacs
     import Networks
-    import Stdcbench
+#    import Stdcbench
     print(str(eval(PREFIX+".name_"+str(c))) + "[" + str(len(eval(PREFIX+".V_"+str(c)))) + "," + str(len(eval(PREFIX+".E_"+str(c))))+"]")
 
 def skip(PREFIX, c, f):
@@ -21,7 +21,7 @@ def skip(PREFIX, c, f):
     import Zoo
     import Dimacs
     import Networks
-    import Stdcbench
+#    import Stdcbench
     if f(len(eval(PREFIX+".V_"+str(c))), len(eval(PREFIX+".E_"+str(c)))):
         return True
     return False
