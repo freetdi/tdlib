d71007b9b2cc6ac5f235d0e77f87ae661ef61c38
Felix Salfelder <felix@salfelder.org>
2021-10-12 00:00:00 +0100

    bump numbering

--- src/numbering.hpp
+++ src/numbering.hpp
@@ -19,6 +19,8 @@
 #define TREEDEC_NUMBERING_H
 
 #include <boost/graph/graph_traits.hpp>
+#include "graph_traits.hpp"
+// #include <boost/property_map/property_map.hpp> ?
 // inspired by boost::mindegree
 // (but more graph-centric)
 
@@ -29,21 +31,28 @@ namespace draft{
 template<class G_t, class T=typename boost::graph_traits<G_t>::vertices_size_type>
 class NUMBERING_1 {
 public:
+	enum ORD_FLAGS{
+	  oDefault,
+	  oFlatten,
+	  oTree,
+     oAuto
+	};
 	static_assert(std::is_unsigned<T>::value);
-    typedef typename boost::property_map<G_t, boost::vertex_index_t>::type idmap_type;
-    typedef boost::graph_traits<G_t> GraphTraits;
-//	 typedef typename GraphTraits::vertices_size_type value_type;
-	 typedef typename GraphTraits::vertex_descriptor vertex_descriptor;
-	 typedef T value_type;
-	 typedef std::vector<value_type> container_type;
+	typedef typename boost::property_map<G_t, boost::vertex_index_t>::type idmap_type;
+	typedef boost::graph_traits<G_t> GraphTraits;
+	//	 typedef typename GraphTraits::vertices_size_type value_type;
+	typedef typename GraphTraits::vertex_descriptor vertex_descriptor;
+	typedef T value_type;
+	typedef std::vector<value_type> container_type;
 
 /* TODO: cleanup
 private: // don't use.
 	 NUMBERING_1(){ unreachable(); }
 	 NUMBERING_1( const NUMBERING_1&){ unreachable(); }
 */
+public:
+	NUMBERING_1(){ untested(); }
 private:
-	NUMBERING_1(){ unreachable(); }
 	NUMBERING_1(const NUMBERING_1&){ unreachable(); }
 public:
 	// resoncsturct numbering from order and sns
@@ -110,7 +119,6 @@ public:
  // "num + i > max_num;"
 		return i + value_type(-_current) > _data.size();
 
-		//  return -current + i > size(); }
 	}
 	void init(value_type i=1) {
 		// hack. should be -1 already.
@@ -132,6 +140,30 @@ public:
 		return is_before(w, v);
 	}
 
+	template<class S>
+	vertex_descriptor find_parent(vertex_descriptor v, S const& sns) const{ itested();
+		while(sns[v] <= 0){ untested();
+			trace3("fp", v, short(_data[v]), sns[v]);
+			v = - _data[v] - 1;
+		}
+		return v;
+	}
+
+	void resize(size_t v) {
+		untested();
+		incomplete();
+		_data.resize(v);
+	}
+	long operator[](vertex_descriptor v) const {
+		auto id = get(_idmap, v);
+		return value_type(-1) - _data[id];
+		// return _data[v];
+	}
+
+	size_t size() const {
+		return _data.size();
+	}
+
 	// is_before(any_numbered, any_not_numbered) -> true
 	// otherwise: -5 is before -10 ...
 	bool is_before(vertex_descriptor v, vertex_descriptor w) const {
@@ -141,7 +173,8 @@ public:
 		return value_type(-1) - _current;
 	}
 	void indistinguishable(vertex_descriptor i, vertex_descriptor j) {
-		_data[get(_idmap, i)] = -(get(_idmap, j) + 1 /*offset?*/);
+		auto idx = get(_idmap, i);
+		_data[idx] = -value_type(get(_idmap, j) + 1) /*offset?*/;
 	}
 	value_type get_position(vertex_descriptor v) const{
 //		trace2("NUMBERING hack", v, sizeof(value_type));
@@ -154,7 +187,23 @@ public:
 	}
 
 	template<class O, class S>
-	void get_ordering(O& ord, S& supernode_by_idx) {
+	void get_ordering(O& ord, S& sns, ORD_FLAGS o=oDefault) { untested();
+		switch(o){
+		case oDefault:
+			return get_ordering(ord, sns, _ord_mode);
+		case oFlatten:
+			untested();
+			return get_ordering_(ord, sns);
+		case oTree:
+			untested();
+			return get_ordering2(ord, sns);
+		case oAuto:
+			incomplete();
+		}
+	}
+private:
+	template<class O, class S>
+	void get_ordering_(O& ord, S& supernode_by_idx) {
 
 		typedef typename O::value_type ovt;
 		static_assert(std::is_signed<ovt>::value); // BUG
@@ -166,21 +215,35 @@ public:
 		unsigned long i;
 
 		// stash numbering in ord
+		// destroy negative supernode entries. (why?)
+		// forget 0 size nodes.
 		for (i = 0; i < n; ++i) {
-			trace3("bp in", i, value_type(-_data[i]), supernode_by_idx[i]);
+				auto pp = - ( ovt(std::numeric_limits<value_type>::max()) - _data[i] ) - 1;
+				pp = (pp>0)?(-1):pp;
+			trace4("bp in", i, value_type(-_data[i]), supernode_by_idx[i], pp);
 			diff_t size = supernode_by_idx[i];
-			if ( size < 0 ) { untested();
-				// reachable? (second call?)
-			}else if ( size == 0 ) {
+		///	if ( size < 0 ) { untested();
+		///		// don't touch supernode? (good?)
+		///		// reachable? (second call?)
+		///	}else
+			if ( size <= 0 ) {
+				// the link to the parent is in _data...
+				// (should it be in supernode?)
+				trace4("bp record sup?", i, ord[i], _data[i], sizeof(value_type));
 				ord[i] = - ( ovt(std::numeric_limits<value_type>::max()) - _data[i] ) - 1;
 				supernode_by_idx[i] = ord[i] + 1;
-				trace1("bp record sup", std::numeric_limits<value_type>::max());
 				trace4("bp record sup", i, ord[i], _data[i], sizeof(value_type));
-				trace1("bp record sup", supernode_by_idx[i]);
 			} else {
 				// not a supernode. now ord > 0.
             _data[i] = value_type(- _data[i] - 1); // final value.
             ord[i] = _data[i] + 1;
+				trace4("bp...", i, ord[i], _data[i], sizeof(value_type));
+			}
+		}
+		for (i = 0; i < n; ++i) {
+			if ( ord[i] > 0 ){
+				// all set.
+				trace2("bp ord set", i, ord[i]);
 			}
 		}
 
@@ -197,6 +260,7 @@ public:
 			while ( ord[parent] < 0 ) {
 				parent = - ord[parent] - 1;
 			}
+			trace2("bp find", i, parent);
 
 			long root = parent;
 			assert(ord[root]>=0);
@@ -207,9 +271,13 @@ public:
 
 			// redirect children.
 			parent = i;
-			while (ord[parent] < 0) {
+			auto op = ord[parent];
+
+			while (op < 0) {
+				trace2("bp set", parent, -root-1);
 				ord[parent] = - root - 1;
-				parent = - ord[parent] - 1;
+				parent = - op - 1;
+				op = ord[parent];
 			}
 		}
 
@@ -223,9 +291,138 @@ public:
 		for (unsigned num=0; num < n; num++) {
 			trace4("bp", num, _data[num], ord[num], supernode_by_idx[ord[num]]);
 		}
-	} // build_permutation()
+	} // get_ordering()
+
+	template<class O, class S>
+	void get_ordering2(O& ord, S& supernode_by_idx) {
+		auto& sns = supernode_by_idx;
+
+		typedef typename O::value_type ovt;
+		static_assert(std::is_signed<ovt>::value); // BUG
+		static_assert(std::is_signed<typename S::value_type>::value); // BUG
+
+		// collect the permutation info
+		auto n = _data.size();
+		typedef long diff_t;
+		unsigned long i;
+		trace1("get_ordering2", n);
+
+		// collect root nodes.
+		for (i = 0; i < n; ++i) {
+			assert(ord[i] == 0);
+			auto pp = - _data[i] - 1;
+			pp = (sns[i]<=0)?pp:-1;
+			trace4("bp in", i, value_type(-_data[i]), sns[i], pp);
+			diff_t size = sns[i];
+
+			// sns > 0: root node
+			// sns = 0: leaf node
+			// sns < 0: inner node
+			if ( size >= 1 ) {
+            _data[i] = value_type(- _data[i] - 1); // final position of node i
+            ord[i] = _data[i] + 1; // position of next block underneath.
+				// at least 1. so ord>0 indicates "allocated"
+			}else if ( size == 0 ) {
+				// leaf node, controlled by some other node.
+				ord[i] = - ( ovt(std::numeric_limits<value_type>::max()) - _data[i] ) - 1; // parent
+				assert(ord[i]<0);
+				_data[i] = 1 - sns[i]; // memory size
+			} else {
+				assert(size!=1);
+				ord[i] = - ( ovt(std::numeric_limits<value_type>::max()) - _data[i] ) - 1; // parent of i
+				assert(ord[i]<0);
+				_data[i] = 1 - sns[i]; // memory size
+			}
+		}
+
+		for (i = 0; i < n; ++i) {
+			if ( ord[i] >= 0 ){
+				// all set.
+				trace3("bp ord set", i, ord[i], sns[i]);
+			}else{
+				trace3("bp ord is parent", i, ord[i], sns[i]);
+			}
+		}
+		// fill gaps in _data.
+		for (i = 0; i < n; ++i) {
+			if ( ord[i] >= 0 ){
+				trace3("final _data set", i, ord[i], _data[i]);
+				continue;
+			}else{
+				auto imm_parent = - ord[i] - 1;
+				trace2("bp find", i, imm_parent);
+			}
+
+			// find root. leave a trace in ord.??
+			//  but dont change ord[root].
+			unsigned long chld = i;
+			assert(ord[chld]<0);
+
+			auto p = - ord[chld] - 1;
+			while ( ord[p] < 0 ) { untested();
+				auto nup = ord[p]; // go up.
+				trace3("trace", i, p, chld);
+				ord[p] = chld; // leave trace
+				chld = p;
+				p = - nup - 1 ;
+			}
+
+			assert(ord[p]>=0);
+			trace5("bp found root and position", i, p, sns[p], ord[p], chld);
+			
+			// go down
+			while(true){
+				auto s = _data[chld];
+				auto next =  ord[chld];
+				assert(_data[chld] < n || long(chld)==long(i));
+
+				_data[chld] = ord[p];
+
+				if (chld==i){
+					ord[chld] = ord[p] + 1;
+					ord[p] += s;
+					// assert(s==sns[i]);
+					trace2("bp leaf", i, sns[i]);
+					break;
+				}else{ untested();
+					// assert(_data[chld] == sns[chld]); // no.
+					trace5("bp noleaf", chld, p, i, sns[chld], ord[p]);
+					ord[chld] = ord[p] + 1;
+					ord[p] += s;
+				}
+
+				p = chld;
+				chld = next;
+			}
+
+		}
+
+		// copy back into ord
+		for (i = 0; i < n; i++) { itested();
+			value_type num = _data[i];
+			ord[num] = i;
+//			trace3("bp data?", i, _data[i], num);
+			_data[i] = -num-1;
+		}
+		for (unsigned num=0; num < n; num++) { itested();
+			trace4("bp go2 out", num, _data[num], ord[num], supernode_by_idx[ord[num]]);
+		}
+	} // get_ordering2()
+
+public:
+// 	bool is_root(vertex_descriptor v) const{
+// 		return sns...
+// 	}
+	void set_mode_tree(){
+		_ord_mode = oTree;
+	}
+	bool is_mode_tree() const{
+		return _ord_mode == oTree;
+	}
+
 private:
 	value_type _current;
+	ORD_FLAGS _ord_mode{oFlatten};
 public: // BUG
 	container_type _data;
 	idmap_type _idmap;
