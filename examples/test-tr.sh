0a4fe53d06a8ce548f0af0d5c45e21e8a007c4ab
Felix Salfelder <felix@salfelder.org>
2024-10-16 00:00:00 +0000

    cleanup test-tr

--- examples/test-tr.sh
+++ examples/test-tr.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-set -e
+set -e -x
 
 out=$( mktemp )
-
 alg=--tr
 
-./tdecomp $alg < ${srcdir}/gr/h.gr > $out
-./td-validate ${srcdir}/gr/h.gr $out
-./tdecomp $alg < ${srcdir}/gr/path.gr | grep -v incomplete > $out
-./td-validate ${srcdir}/gr/path.gr $out
-./tdecomp $alg < ${srcdir}/gr/path10.gr | grep -v incomplete > $out
-./td-validate ${srcdir}/gr/path10.gr $out
-./tdecomp $alg < ${srcdir}/gr/clique3.gr | grep -v incomplete > $out
-./td-validate ${srcdir}/gr/clique3.gr $out
-./tdecomp $alg < ${srcdir}/gr/clique4.gr | grep -v incomplete > $out
-./td-validate ${srcdir}/gr/clique4.gr $out
+test() {
+	echo $alg ${srcdir}/gr/$1.gr
+	./tdecomp $alg < ${srcdir}/gr/$1.gr | grep -v incomplete > $out
+	./td-validate ${srcdir}/gr/$1.gr $out
+}
+
+test h
+test path
+test path10
+test clique3
+test clique4
 
 rm $out
